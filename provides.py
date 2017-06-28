794dba77627e576f1cb293701ede83a5cb5e3162
Mathijs Moerman <mathijs.moerman@qrama.io>
2017-06-28 11:53:41 +0200

    Working initial version

---
+++ provides.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+# Copyright (C) 2017  Qrama, developed by Tengu-team
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# pylint: disable=c0111,c0301,c0325, r0903,w0406
+from charms.reactive import hook, RelationBase, scopes
+
+
+class SSLTerminationProvides(RelationBase):
+    scope = scopes.UNIT
+
+    @hook('{provides:ssl-termination}-relation-{joined}')
+    def joined(self):
+        conv = self.conversation()
+        conv.remove_state('{relation_name}.removed')
+        conv.set_state('{relation_name}.connected')
+
+    @hook('{requires:ssl-termination}-relation-{changed}')
+    def changed(self):
+        for conv in self.conversations():
+            conv = self.conversation()
+            data = {
+                'service': conv.get_remote('service'),
+                'fqdns': conv.get_remote('fqdns'),
+                'private_ips': conv.get_remote('private_ips'),
+                'basic_auth': conv.get_remote('basic_auth'),
+                'loadbalancing': conv.get_remote('loadbalancing')
+            }
+            if data['service']:
+                conv.set_state('{relation_name}.available')
+
+    @hook('{provides:ssl-termination}-relation-{broken,departed}')
+    def broken(self):
+        conv = self.conversation()
+        conv.remove_state('{relation_name}.connected')
+        conv.remove_state('{relation_name}.available')
+        conv.set_state('{relation_name}.removed')
+
+    def get_data(self):
+        data = []
+        for conv in self.conversations():
+            data.append({
+                'service': conv.get_remote('service'),
+                'fqdns': conv.get_remote('fqdns'),
+                'private_ips': conv.get_remote('private_ips'),
+                'basic_auth': conv.get_remote('basic_auth'),
+                'loadbalancing': conv.get_remote('loadbalancing')
+            })
+        return data
+
+    def check_status(self):
+        self.changed()
---
+++ requires.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+# Copyright (C) 2017  Qrama, developed by Tengu-team
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# pylint: disable=c0111,c0301,c0325, r0903,w0406
+from charms.reactive import hook, RelationBase, scopes
+
+
+class SSLTerminationRequires(RelationBase):
+    scope = scopes.UNIT
+
+    @hook('{requires:ssl-termination}-relation-{joined}')
+    def joined(self):
+        conv = self.conversation()
+        conv.remove_state('{relation_name}.removed')
+        conv.set_state('{relation_name}.connected')
+
+    @hook('{requires:ssl-termination}-relation-{changed}')
+    def changed(self):
+        for conv in self.conversations():
+            data = {
+                'service': conv.get_local('service'),
+                'fqdns': conv.get_local('fqdns'),
+                'private_ips': conv.get_local('private_ips'),
+                'loadbalancing': conv.get_local('loadbalancing')
+            }
+            if data['service']:
+                conv.set_state('{relation_name}.available')
+
+    @hook('{requires:ssl-termination}-relation-{departed,broken}')
+    def broken(self):
+        conv = self.conversation()
+        conv.remove_state('{relation_name}.connected')
+        conv.remove_state('{relation_name}.available')
+        conv.set_state('{relation_name}.removed')
+
+    def request_proxy(self, service_name, array_fqdns, array_private_ips, basic_auth=None, loadbalancing=None):
+        for conv in self.conversations():
+            relation_info = {
+                'service': service_name,
+                'fqdns': array_fqdns,
+                'private_ips': array_private_ips,
+                'basic_auth': basic_auth if basic_auth else [],
+                'loadbalancing': loadbalancing if loadbalancing else '',
+                }
+            conv.set_local(**relation_info)
+            conv.set_remote(**relation_info)
+        self.changed()
